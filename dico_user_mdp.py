816fd4cb53108a629ae5374227434ec3c6ed4d16
Julian bdt <bidaultjulian@gmail.com>
2025-01-06 12:10:19 +0100

    Create dico_user_mdp.py

---
+++ dico_user_mdp.py
@@ -0,0 +1,19 @@
+import json
+
+def validate_user(login,pwd):
+    if login not in user_logins_values:
+        dico_users[login] = pwd
+
+
+
+
+
+dico_users = {
+    "login1" : "pwd1",
+    "login2" : "pwd2",
+    "login3" : "pwd3",
+    "login4" : "pwd4",
+    "login5" : "pwd5",
+    "login6" : "pwd6",
+
+    }
