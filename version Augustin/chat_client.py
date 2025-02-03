bc141b06b01fb2b3c1af7e5c36d87e58107bfcdd
Julian bdt <bidaultjulian@gmail.com>
2025-02-03 11:49:26 +0100

    Update chat_client.py

--- version Augustin/chat_client.py
+++ version Augustin/chat_client.py
@@ -2,12 +2,13 @@ import socket
 import threading
 import tkinter as tk
 from tkinter import scrolledtext
+import time  # Ajout de l'importation du module time
 
-def receive_message():
+def handle_client():
     while True:
         message = client_socket.recv(1024).decode('utf-8')
         chat_area.config(state=tk.NORMAL)
-        chat_area.insert(tk.END, "Serveur: " + message + "\n")
+        chat_area.insert(tk.END, "Client: " + message + "\n")
         chat_area.config(state=tk.DISABLED)
 
 def send_message():
@@ -18,11 +19,28 @@ def send_message():
     chat_area.insert(tk.END, "Vous: " + message + "\n")
     chat_area.config(state=tk.DISABLED)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('adresse_ip_du_serveur', 12345))
+def start_server():
+    global client_socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 12345))
+    server_socket.listen(1)
+    chat_area.insert(tk.END, "En attente de connexion...\n")
+    
+    client_socket, addr = server_socket.accept()
+    chat_area.insert(tk.END, f"Connexion de {addr}\n")
+
+    thread = threading.Thread(target=handle_client)
+    thread.start()
+
+def broadcast_ip():
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    while True:
+        broadcast_socket.sendto(b"SERVER_IP", ('<broadcast>', 12345))
+        time.sleep(5)
 
 root = tk.Tk()
-root.title("Chat Client")
+root.title("Chat Serveur")
 
 chat_area = scrolledtext.ScrolledText(root, state=tk.DISABLED)
 chat_area.pack(padx=10, pady=10)
@@ -34,7 +52,5 @@ message_entry.bind("<Return>", lambda event: send_message())
 send_button = tk.Button(root, text="Envoyer", command=send_message)
 send_button.pack(padx=10, pady=10)
 
-thread = threading.Thread(target=receive_message)
-thread.start()
+thread = threading.Thread(target=start_server)
 
-root.mainloop()
